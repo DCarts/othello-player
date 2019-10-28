b87fc8c01757c143027de433b5c70882bad2ee1e
Daniel Souza <dcarts@users.noreply.github.com>
2019-10-28 14:54:36 -0300

    Metodos de bitboard.py otimizados

--- models/players/bitboard.py
+++ models/players/bitboard.py
@@ -5,6 +5,9 @@ from models.board import Board
   
 ZERO = i64(0)
 ONE = i64(1)
+TWO = i64(2)
+THREE = i64(3)
+FOUR = i64(4)
 SIZE = i64(8)
 SIZEM1 = i64(7)
 SIZEP1 = i64(9)
@@ -17,6 +20,11 @@ BB_UP_LEFT = i64(0xFEFEFEFEFEFEFE00)
 BB_UP_RIGHT = i64(0x7F7F7F7F7F7F7F00)
 BB_DOWN_LEFT = i64(0x00FEFEFEFEFEFEFE)
 BB_DOWN_RIGHT = i64(0x007F7F7F7F7F7F7F)
+
+BB_UPDOWN = BB_UP & BB_DOWN
+BB_LEFTRIGHT = BB_LEFT & BB_RIGHT
+BB_UPLEFTDOWNRIGHT = BB_UP_LEFT & BB_DOWN_RIGHT
+BB_UPRIGHTDOWNLEFT = BB_UP_RIGHT & BB_DOWN_LEFT
 
 def next_up(x):
   return (x << SIZE) & BB_UP
@@ -44,6 +52,31 @@ next_move_directions = (next_up,
          next_down_left,
          next_down_right)
 
+def print_bbn(bbn):
+  """imprime um numero 64 bits em bitboard 8x8"""
+  mask = i64(1) << i64(63)
+  for i in range(8):
+    line = ""
+    for j in range(8):
+      line += ("1" if bbn & mask else "0")
+      mask >>= i64(1)
+    print line
+
+def print_pov(bb):
+  """imprime um tabuleiro do meu ponto de vista"""
+  mask = i64(1) << i64(63)
+  for i in range(8):
+    line = ""
+    for j in range(8):
+      if (bb.me & mask):
+        line += 'M'
+      elif (bb.op & mask):
+        line += 'O'
+      else:
+        line += '.'
+      mask >>= i64(1)
+    print line
+
 def bitcount(x):
   """conta quantos bits '1' o numero x possui"""
   count = 0.0
@@ -69,19 +102,88 @@ def find_moves(bb):
   empty = bb.get_empty()
   
   moves = i64(0)
-  for next_dir in next_move_directions:
-    candidates = op & next_dir(me)
-    while (candidates):
-      moves     |= empty & next_dir(candidates)
-      candidates = op    & next_dir(candidates)
+  
+  viable      = op & BB_UPDOWN
+  candidates  = viable & (me << SIZE)
+  candidates |= viable & (candidates << SIZE)
+  candidates |= viable & (candidates << SIZE)
+  candidates |= viable & (candidates << SIZE)
+  candidates |= viable & (candidates << SIZE)
+  candidates |= viable & (candidates << SIZE)
+  moves      |= empty  & (candidates << SIZE)
+  
+  viable      = op & BB_UPDOWN
+  candidates  = viable & (me >> SIZE)
+  candidates |= viable & (candidates >> SIZE)
+  candidates |= viable & (candidates >> SIZE)
+  candidates |= viable & (candidates >> SIZE)
+  candidates |= viable & (candidates >> SIZE)
+  candidates |= viable & (candidates >> SIZE)
+  moves      |= empty  & (candidates >> SIZE)
+  
+  viable      = op & BB_LEFTRIGHT
+  candidates  = viable & (me << ONE)
+  candidates |= viable & (candidates << ONE)
+  candidates |= viable & (candidates << ONE)
+  candidates |= viable & (candidates << ONE)
+  candidates |= viable & (candidates << ONE)
+  candidates |= viable & (candidates << ONE)
+  moves      |= empty  & (candidates << ONE)
+  
+  viable      = op & BB_LEFTRIGHT
+  candidates  = viable & (me >> ONE)
+  candidates |= viable & (candidates >> ONE)
+  candidates |= viable & (candidates >> ONE)
+  candidates |= viable & (candidates >> ONE)
+  candidates |= viable & (candidates >> ONE)
+  candidates |= viable & (candidates >> ONE)
+  moves      |= empty  & (candidates >> ONE)
+  
+  viable      = op & BB_UPLEFTDOWNRIGHT
+  candidates  = viable & (me << SIZEP1)
+  candidates |= viable & (candidates << SIZEP1)
+  candidates |= viable & (candidates << SIZEP1)
+  candidates |= viable & (candidates << SIZEP1)
+  candidates |= viable & (candidates << SIZEP1)
+  candidates |= viable & (candidates << SIZEP1)
+  moves      |= empty  & (candidates << SIZEP1)
+  
+  viable      = op & BB_UPRIGHTDOWNLEFT
+  candidates  = viable & (me << SIZEM1)
+  candidates |= viable & (candidates << SIZEM1)
+  candidates |= viable & (candidates << SIZEM1)
+  candidates |= viable & (candidates << SIZEM1)
+  candidates |= viable & (candidates << SIZEM1)
+  candidates |= viable & (candidates << SIZEM1)
+  moves      |= empty  & (candidates << SIZEM1)
+  
+  viable      = op & BB_UPRIGHTDOWNLEFT
+  candidates  = viable & (me >> SIZEM1)
+  candidates |= viable & (candidates >> SIZEM1)
+  candidates |= viable & (candidates >> SIZEM1)
+  candidates |= viable & (candidates >> SIZEM1)
+  candidates |= viable & (candidates >> SIZEM1)
+  candidates |= viable & (candidates >> SIZEM1)
+  moves      |= empty  & (candidates >> SIZEM1)
+  
+  viable      = op & BB_UPLEFTDOWNRIGHT
+  candidates  = viable & (me >> SIZEP1)
+  candidates |= viable & (candidates >> SIZEP1)
+  candidates |= viable & (candidates >> SIZEP1)
+  candidates |= viable & (candidates >> SIZEP1)
+  candidates |= viable & (candidates >> SIZEP1)
+  candidates |= viable & (candidates >> SIZEP1)
+  moves      |= empty  & (candidates >> SIZEP1)
+  
   return moves
 
 def bits_iter(bbn):
   """cria um iterable para cada bit '1' da entrada"""
   bits = []
+  append = bits.append # atalho pra funcao bits.append(), fica mais rapido assim
   while (bbn):
     without_lsb = bbn & (bbn-ONE)
-    bits.append(bbn - without_lsb)
+    append(bbn - without_lsb)
     bbn = without_lsb
   return bits
 
@@ -133,32 +235,6 @@ def makeBoard(bb):
     board.board[convert_move(move).x][convert_move(move).y] = board.WHITE
   return board
 
-def print_bbn(bbn):
-  """imprime um numero 64 bits em bitboard 8x8"""
-  mask = i64(1) << i64(63)
-  for i in range(8):
-    line = ""
-    for j in range(8):
-      line += ("1" if bbn & mask else "0")
-      mask >>= i64(1)
-    print line
-
-def print_pov(bb):
-  """imprime um tabuleiro do meu ponto de vista"""
-  mask = i64(1) << i64(63)
-  for i in range(8):
-    line = ""
-    for j in range(8):
-      if (bb.me & mask):
-        line += 'M'
-      elif (bb.op & mask):
-        line += 'O'
-      else:
-        line += '.'
-      mask >>= i64(1)
-    print line
-
-
 class BitBoard:
   """classe que representa uma bitboard"""
 
@@ -183,17 +259,79 @@ class BitBoard:
     """aplica um movimento na bitboard"""
     empty = self.get_empty()
 
-    for next_dir in next_move_directions:
-      walk = next_dir(move)
-      line = i64(0)
-      while (walk & self.op):
-        line |= walk
-        walk = next_dir(walk)
-      if (walk & self.me):
-        self.op &= ~line
-        self.me |= line
-    self.me |= move
+    walk = (move << SIZE) & BB_UP
+    line = i64(0)
+    while (walk & self.op):
+      line |= walk
+      walk = (walk << SIZE) & BB_UP
+    if (walk & self.me):
+      self.op &= ~line
+      self.me |= line
 
+    walk = (move >> SIZE) & BB_DOWN
+    line = i64(0)
+    while (walk & self.op):
+      line |= walk
+      walk = (walk >> SIZE) & BB_DOWN
+    if (walk & self.me):
+      self.op &= ~line
+      self.me |= line
+
+    walk = (move << ONE) & BB_LEFT
+    line = i64(0)
+    while (walk & self.op):
+      line |= walk
+      walk = (walk << ONE) & BB_LEFT
+    if (walk & self.me):
+      self.op &= ~line
+      self.me |= line
+
+    walk = (move >> ONE) & BB_RIGHT
+    line = i64(0)
+    while (walk & self.op):
+      line |= walk
+      walk = (walk >> ONE) & BB_RIGHT
+    if (walk & self.me):
+      self.op &= ~line
+      self.me |= line
+
+    walk = (move << SIZEP1) & BB_UP_LEFT
+    line = i64(0)
+    while (walk & self.op):
+      line |= walk
+      walk = (walk << SIZEP1) & BB_UP_LEFT
+    if (walk & self.me):
+      self.op &= ~line
+      self.me |= line
+
+    walk = (move << SIZEM1) & BB_UP_RIGHT
+    line = i64(0)
+    while (walk & self.op):
+      line |= walk
+      walk = (walk << SIZEM1) & BB_UP_RIGHT
+    if (walk & self.me):
+      self.op &= ~line
+      self.me |= line
+
+    walk = (move >> SIZEM1) & BB_DOWN_LEFT
+    line = i64(0)
+    while (walk & self.op):
+      line |= walk
+      walk = (walk >> SIZEM1) & BB_DOWN_LEFT
+    if (walk & self.me):
+      self.op &= ~line
+      self.me |= line
+
+    walk = (move >> SIZEP1) & BB_DOWN_RIGHT
+    line = i64(0)
+    while (walk & self.op):
+      line |= walk
+      walk = (walk >> SIZEP1) & BB_DOWN_RIGHT
+    if (walk & self.me):
+      self.op &= ~line
+      self.me |= line
+
+    self.me |= move
     return self
 
   def change_player(self):
